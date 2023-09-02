dcb75ffe8c575d09fabb505db2b8c39d03d933c0
Sodara Sou <sodarasou168@gmail.com>
2023-09-02 22:56:00 +0700

    Create home route

--- lib/Pages/page_route.dart
+++ lib/Pages/page_route.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:student_app/xcore.dart';
 
 class PageRouter {
+  static const String home = '/home';
   static const String splashScreen = '/splashScreen';
   static const String student = '/student';
   static const String registerForm = '/registerForm';
@@ -17,6 +18,11 @@ class PageRouter {
     GetPage(
       name: splashScreen,
       page: () => const SplashScreen(),
+    ),
+    GetPage(
+      name: home,
+      page: () => const HomeView(),
+      binding: HomeBinding(),
     ),
     GetPage(
       name: student,
