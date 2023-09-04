08cadf0d6101d006b09a3e6fa50786642f3eb267
Sodara Sou <sodarasou168@gmail.com>
2023-09-04 22:14:26 +0700

    Add temporary log out button

--- lib/Pages/Home/home_controller.dart
+++ lib/Pages/Home/home_controller.dart
@@ -1,8 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:student_app/xcore.dart';
 
 class HomeController extends GetxController {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
   void toStudentPage() {
     Get.toNamed(PageRouter.student);
+  }
+
+  Future<void> signOut() async {
+    try {
+      await auth.signOut();
+      Get.toNamed(PageRouter.login);
+    } catch (e) {
+      Get.snackbar('Error', e.toString());
+    }
   }
 }
--- lib/Pages/Home/home_view.dart
+++ lib/Pages/Home/home_view.dart
@@ -8,18 +8,43 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text('Student App'),
-        ),
-      ),
-      body: SafeArea(
-        child: Center(
-          child: ElevatedButton(
-            onPressed: () => controller.toStudentPage(),
-            child: const Text('View all students'),
+      body: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 2.5,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      'Hi',
+                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                            fontWeight: FontWeight.w200,
+                          ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
-        ),
+          Expanded(
+            child: Container(
+              color: Colors.transparent,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                  color: kOtherColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(kDefaultPadding * 3),
+                    topRight: Radius.circular(kDefaultPadding * 3),
+                  ),
+                ),
+                child: ElevatedButton(onPressed: () => controller.signOut(), child: const Text('Sign Out')),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
