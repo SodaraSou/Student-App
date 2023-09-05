5722e2ca01ace32c6296b0765ab7f33d872e36d6
Sodara Sou <sodarasou168@gmail.com>
2023-09-05 22:22:25 +0700

    Finished sign up

--- lib/Pages/sign_up/sign_up_controller.dart
+++ lib/Pages/sign_up/sign_up_controller.dart
@@ -1,5 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:student_app/xcore.dart';
 
 class SignUpController extends GetxController {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  DatabaseReference ref = FirebaseDatabase.instance.ref();
+  final firstName = TextEditingController();
+  final lastName = TextEditingController();
+  final department = TextEditingController();
+  final email = TextEditingController();
+  final password = TextEditingController();
+  final confirmPassword = TextEditingController();
+  String userId = '';
 
+  Future<void> registerUser() async {
+    // Create user account
+    try {
+      await auth.createUserWithEmailAndPassword(
+        email: email.text.trim(),
+        password: password.text.trim(),
+      );
+    } catch (e) {
+      Get.snackbar('Error', e.toString());
+    }
+    // Create user database
+    userId = auth.currentUser!.uid;
+    try {
+      await ref.child('data').child(userId).set({
+        'email': email.text.trim(),
+        'password': password.text.trim(),
+        'firstName': firstName.text.trim(),
+        'lastName': lastName.text.trim(),
+        'department': department.text.trim(),
+      });
+    } catch (e) {
+      Get.snackbar('Error', e.toString());
+    }
+    Get.offNamed(PageRouter.home);
+  }
 }
--- lib/Pages/sign_up/sign_up_view.dart
+++ lib/Pages/sign_up/sign_up_view.dart
@@ -39,11 +39,136 @@ class SignUpView extends GetView<SignUpController> {
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height,
               decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(kDefaultPadding * 3),
-                    topRight: Radius.circular(kDefaultPadding * 3),
-                  ),
-                  color: kOtherColor),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(kDefaultPadding * 3),
+                  topRight: Radius.circular(kDefaultPadding * 3),
+                ),
+                color: kOtherColor,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(kDefaultPadding),
+                child: Column(
+                  children: [
+                    Form(
+                      child: Column(
+                        children: [
+                          TextField(
+                            keyboardType: TextInputType.name,
+                            style: const TextStyle(
+                              color: kTextBlackColor,
+                              fontSize: 17,
+                              fontWeight: FontWeight.w300,
+                            ),
+                            decoration: const InputDecoration(
+                              labelText: 'First Name',
+                            ),
+                            controller: controller.firstName,
+                          ),
+                          const SizedBox(
+                            height: kDefaultPadding,
+                          ),
+                          TextField(
+                            keyboardType: TextInputType.name,
+                            style: const TextStyle(
+                              color: kTextBlackColor,
+                              fontSize: 17,
+                              fontWeight: FontWeight.w300,
+                            ),
+                            decoration: const InputDecoration(
+                              labelText: 'Last Name',
+                            ),
+                            controller: controller.lastName,
+                          ),
+                          const SizedBox(
+                            height: kDefaultPadding,
+                          ),
+                          TextField(
+                            keyboardType: TextInputType.name,
+                            style: const TextStyle(
+                              color: kTextBlackColor,
+                              fontSize: 17,
+                              fontWeight: FontWeight.w300,
+                            ),
+                            decoration: const InputDecoration(
+                              labelText: 'Department',
+                            ),
+                            controller: controller.department,
+                          ),
+                          const SizedBox(
+                            height: kDefaultPadding,
+                          ),
+                          TextField(
+                            keyboardType: TextInputType.emailAddress,
+                            style: const TextStyle(
+                              color: kTextBlackColor,
+                              fontSize: 17,
+                              fontWeight: FontWeight.w300,
+                            ),
+                            decoration: const InputDecoration(
+                              labelText: 'Email',
+                            ),
+                            controller: controller.email,
+                          ),
+                          const SizedBox(
+                            height: kDefaultPadding,
+                          ),
+                          TextField(
+                            keyboardType: TextInputType.visiblePassword,
+                            style: const TextStyle(
+                              color: kTextBlackColor,
+                              fontSize: 17,
+                              fontWeight: FontWeight.w300,
+                            ),
+                            decoration: const InputDecoration(
+                              labelText: 'Password',
+                            ),
+                            controller: controller.password,
+                          ),
+                          const SizedBox(
+                            height: kDefaultPadding,
+                          ),
+                          TextField(
+                            keyboardType: TextInputType.visiblePassword,
+                            style: const TextStyle(
+                              color: kTextBlackColor,
+                              fontSize: 17,
+                              fontWeight: FontWeight.w300,
+                            ),
+                            decoration: const InputDecoration(
+                              labelText: 'Confirm Password',
+                            ),
+                            controller: controller.confirmPassword,
+                          ),
+                          const SizedBox(
+                            height: kDefaultPadding,
+                          ),
+                          InkWell(
+                            onTap: () => controller.registerUser(),
+                            child: Container(
+                              width: double.infinity,
+                              height: 60,
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.circular(kDefaultPadding),
+                                color: kPrimaryColor,
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  'Sign Up',
+                                  style: TextStyle(
+                                    fontSize: 17,
+                                    color: kTextWhiteColor,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
