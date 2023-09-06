d013338ce35f23bd66607736ec8f73df0c53ceec
Sodara Sou <sodarasou168@gmail.com>
2023-09-06 15:18:14 +0700

    Minor fixed

--- lib/Pages/Login/login_controller.dart
+++ lib/Pages/Login/login_controller.dart
@@ -5,16 +5,8 @@ import 'package:student_app/xcore.dart';
 
 class LoginController extends GetxController {
   final FirebaseAuth auth = FirebaseAuth.instance;
-  Rxn<User> user = Rxn<User>();
-
   final email = TextEditingController();
   final password = TextEditingController();
-
-  @override
-  void onInit() {
-    super.onInit();
-    user.bindStream(auth.authStateChanges());
-  }
 
   void toSignUp() {
     Get.toNamed(PageRouter.signUp);
